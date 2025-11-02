853ac6a0dade42e031881166c5c4b45a7c5e805e
Oceanami <dangtuanhung2354@gmail.com>
2025-11-02 16:33:54 +0700

    Fix format specifier for double output

--- BaiTapChuong1+2/bai3.cpp
+++ BaiTapChuong1+2/bai3.cpp
@@ -10,6 +10,7 @@ int main()
 	printf("\n");
 	B=sin(a)+sqrt(a);
 	printf("So da nhap a=%6d\n", a);
-	printf("Gia tri bieu thuc B=%.6lf\n", B);
+	printf("Gia tri bieu thuc B=%.6f\n", B);
 	return 0; 
 }
+
