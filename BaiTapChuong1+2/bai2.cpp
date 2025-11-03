b382d07d6a7a9c83a9be8b7d0b446f06b87a580e
Oceanami <dangtuanhung2354@gmail.com>
2025-11-03 19:04:32 +0700

    Refactor bai2.cpp for clarity and precision
    Fixed formatting issues and improved output precision.

--- BaiTapChuong1+2/bai2.cpp
+++ BaiTapChuong1+2/bai2.cpp
@@ -7,9 +7,11 @@ int main()
 	printf("Nhap a=");
 	scanf("%lf", &a);
 	printf("\n");
+	
 	B=5*pow(a,3)-4*pow(a,2)+3*a-2; 
 	printf("So da nhap a=%10.3f\n", a);
-	printf("Gia tri bieu thuc B=%10.6f\n", B);
+	printf("Gia tri bieu thuc B=%10f\n", B);
 	return 0; 
 } 
+
 
