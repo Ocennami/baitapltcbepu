0a920a46f051fcc66df5e1aed4fc68bc48faad72
Oceanami <dangtuanhung2354@gmail.com>
2025-11-03 19:10:12 +0700

    Refactor bai3.cpp for clarity and output changes
    Removed redundant lines and adjusted output format.

--- BaiTapChuong1+2/bai3.cpp
+++ BaiTapChuong1+2/bai3.cpp
@@ -8,9 +8,10 @@ int main()
 	printf("nhap a=");
 	scanf("%d", &a);
 	printf("\n");
-	B=sin(a)+sqrt(a);
+
 	printf("So da nhap a=%6d\n", a);
-	printf("Gia tri bieu thuc B=%.6f\n", B);
+	B=sin(a)+sqrt(a);
+	printf("Gia tri bieu thuc B=%f\n", B);
 	return 0; 
 }
 
